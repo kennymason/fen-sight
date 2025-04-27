ac29d799497ce644a334cc88b4b8b04a189e04be
Kenneth Mason <kennymmase@gmail.com>
2025-04-27 12:47:38 -0400

    CNN and training script; v.1

---
+++ chess_neural_network.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Defines a Convolutional Neural Network (CNN) for chess piece recognition
+class ChessNN(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.conv1 = nn.Conv2d(3, 32, 5) # 3 input channels (RGB), 32 output feature maps, kernel size 5
+    self.pool = nn.MaxPool2d(2, 2) # kernel size 2, stride 2 (same as kernel size, as is default)
+    self.conv2 = nn.Conv2d(32, 64, 5) # 32 input channels, 64 output channels
+    # Output = (62 - 5 + 0)/1 + 1 = 58 -> (58x58)
+    self.fc1 = nn.Linear(64 * 29 * 29, 512) # 64*29*29 size of vector after torch.flatten(), I chose 512 as output size
+    self.fc2 = nn.Linear(512, 128) # 512 input size, 128 output size
+    self.fc3 = nn.Linear(128, 26) # 128 input size, 26 output size (we have 26 classes)
+
+  def forward(self, x):
+    # Input Size = 128 -> (128px x 128px image)
+    x = self.pool(F.relu(self.conv1(x)))
+    # Conv Output Size = ((input_size - kernel_size + 2*padding) / stride) + 1 = (128 - 5 + 0)/1 + 1 = 124 -> (124x124)
+    # Pool Output Size = input_size / kernel_size = 62 -> (124x124 / 2x2 = 62x62)
+    x = self.pool(F.relu(self.conv2(x)))
+    # Conv Output Size = (62 - 5 + 0)/1 + 1 = 58 -> (58x58)
+    # Pool Output Size = 29 -> (29x29)
+    x = torch.flatten(x, 1) # flatten all dimensions except batch
+    # Output Size = channels * height * width = 64 * 29 * 29 = 53,824
+    x = F.relu(self.fc1(x))
+    # Output Size = 512
+    x = F.relu(self.fc2(x))
+    # Output Size = 128
+    x = self.fc3(x)
+    # Output Size = 26 (26 classes)
+    return x
---
+++ train.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from chess_neural_network import ChessNN
+
+# Convolutional Neural Network
+cnn = ChessNN()
+
+# Parameters
+BATCH_SIZE = 4
+DATA_DIR = 'dataset/'
+NUM_WORKERS = 2 # If running on Windows and get a BrokenPipeError, try setting NUM_WORKERS=0
+EPOCHS = 10
+LEARNING_RATE = 0.001
+
+# Transforms
+transform = transforms.Compose([
+  transforms.Resize((128, 128)),
+  transforms.ToTensor(),
+  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# Training image dataset
+trainset = torchvision.datasets.ImageFolder(root=DATA_DIR, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
+
+# Testing image dataset
+testset = torchvision.datasets.ImageFolder(root=DATA_DIR, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
+
+# Loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(cnn.parameters(), lr=LEARNING_RATE, momentum=0.9)
+
+# Loop
+for epoch in range(EPOCHS):
+  running_loss = 0.0
+  for i, data in enumerate(trainloader, 0):
+    # get inputs; data is a list of [inputs, labels]
+    inputs, labels = data
+
+    # zero the parameter gradients
+    optimizer.zero_grad()
+
+    # forward + backward + optimize
+    outputs = cnn(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+
+    # print statistics
+    running_loss += loss.item()
+    if i % 100 == 99: # print every 2000 mini-batches
+      print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+      running_loss = 0.0
+
+# === Save the model ===
+torch.save(cnn.state_dict(), 'model.pth')
+
+print("Finished Training")
